dd19a9a21b92d76cbc1e5569feb0fc8c4719b316
yisongwu <yisongwu@tencent.com>
2020-06-21 14:56:09 +0800

    feat: add Solution_125.java

---
+++ app/src/main/java/com/example/algorithm/leetcode/Solution_147.java
@@ -0,0 +1,19 @@
+package com.example.algorithm.leetcode;
+
+public class Solution_Module {
+    public static void main(String[] args) {
+        Solution_Module solution = new Solution_Module();
+    }
+
+    public int minDistance(String word1, String word2) {
+        return 1;
+    }
+
+    public int mySolution_1(String word1, String word2) {
+        return 1;
+    }
+
+    public int mySolution_2(String word1, String word2) {
+        return 1;
+    }
+}
