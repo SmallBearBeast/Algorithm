2d60da86d82df392735ae41a15d56894622f4b77
Hugo.Wu <smallbearbeast@gmail.com>
2023-07-14 09:00:07 +0800

    feat: Add Solution_1

---
+++ app/src/main/java/com/example/algorithm/leetcode/Solution_1.java
@@ -0,0 +1,102 @@
+package com.example.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ */
+
+public class Solution_1 {
+    public static void main(String[] args) {
+        Solution_1 solution_1 = new Solution_1();
+        int[] result = solution_1.mySolution_2(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(Arrays.toString(result));
+    }
+
+    // O(n2)
+    public int[] mySolution_1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] mySolution_2(int[] nums, int target) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], new ArrayList<Integer>());
+            }
+            map.get(nums[i]).add(i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int anotherKey = target - nums[i];
+            if (map.containsKey(anotherKey)) {
+                List<Integer> anotherIndexList = map.get(anotherKey);
+                int size = anotherIndexList.size();
+                for (int j = 0; j < size; j++) {
+                    int anotherIndex = anotherIndexList.get(j);
+                    if (anotherIndex != i) {
+                        return new int[]{i, anotherIndex};
+                    }
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] mySolution_3(int[] nums, int target) {
+        int[] originNums = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(nums);
+        int startIndex = 0;
+        int endIndex = nums.length - 1;
+        int[] values = new int[2];
+        while (startIndex < endIndex) {
+            int sum = nums[startIndex] + nums[endIndex];
+            if (sum == target) {
+                values[0] = nums[startIndex];
+                values[1] = nums[endIndex];
+                break;
+            } else if (sum > target) {
+                endIndex--;
+            } else {
+                startIndex++;
+            }
+        }
+        int[] result = new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE};
+        for (int i = 0; i < originNums.length; i++) {
+            if (result[0] == Integer.MIN_VALUE && values[0] == originNums[i]) {
+                result[0] = i;
+            } else if (result[1] == Integer.MIN_VALUE && values[1] == originNums[i]) {
+                result[1] = i;
+            }
+        }
+        return result;
+    }
+
+    // 哈希表
+    public int[] mySolution_4(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        return mySolution_4(nums, target);
+    }
+}
--- app/src/main/java/com/example/algorithm/leetcode/Solution_Temp.java
+++ app/src/main/java/com/example/algorithm/leetcode/Solution_Temp.java
@@ -1,71 +1,12 @@
 package com.example.algorithm.leetcode;
 
-import com.example.algorithm.structure.TreeNode;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
-
 public class Solution_Temp {
     public static void main(String[] args) {
-        Solution_Temp solution = new Solution_Temp();
-        List<Integer> list = new ArrayList<>();
-        list.addAll(Arrays.asList(1, 2, 3, 4, 5));
-        list.addAll(list.size(), Arrays.asList(6, 7, 8));
-        list.add(9);
+        Solution_Temp solution_temp = new Solution_Temp();
+        solution_temp.mySolution_1();
     }
 
-    public int climbStairs(int n) {
-        return 1;
-    }
+    public void mySolution_1() {
 
-    public int mySolution(int n) {
-        return 1;
-    }
-
-    public boolean isSameTree_1(TreeNode tree_1, TreeNode tree_2) {
-        if (tree_1 == null && tree_2 == null) {
-            return true;
-        }
-        if (tree_1 != null && tree_2 != null && tree_1.val == tree_2.val) {
-            return isSameTree_1(tree_1.left, tree_2.left) && isSameTree_1(tree_1.right, tree_2.right);
-        }
-        return false;
-    }
-
-    public boolean isSameTree_2(TreeNode tree_1, TreeNode tree_2) {
-        if (tree_1 == null && tree_2 == null) {
-            return true;
-        }
-        if (tree_1 != null && tree_2 != null) {
-            Queue<TreeNode> queue_1 = new LinkedList<>();
-            Queue<TreeNode> queue_2 = new LinkedList<>();
-            queue_1.add(tree_1);
-            queue_2.add(tree_2);
-            while (!queue_1.isEmpty() && !queue_2.isEmpty()) {
-                TreeNode node_1 = queue_1.poll();
-                TreeNode node_2 = queue_2.poll();
-                if (node_1.left != null) {
-                    queue_1.add(node_1.left);
-                }
-                if (node_1.right != null) {
-                    queue_1.add(node_1.left);
-                }
-                if (node_2.left != null) {
-                    queue_2.add(node_2.left);
-                }
-                if (node_2.right != null) {
-                    queue_2.add(node_2.left);
-                }
-                if (node_1.val != node_2.val) {
-                    return false;
-                }
-            }
-            return queue_1.isEmpty() && queue_2.isEmpty();
-        }
-        return false;
     }
 }
