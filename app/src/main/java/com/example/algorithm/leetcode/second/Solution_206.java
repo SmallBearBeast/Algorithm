1d3b5327b4bed1db8a29eba475cce924668d840a
Hugo.Wu <smallbearbeast@gmail.com>
2023-08-10 14:07:11 +0800

    feat: Add second/Solution_206.java

--- app/src/main/java/com/example/algorithm/leetcode/second/Solution_206.java
+++ app/src/main/java/com/example/algorithm/leetcode/second/Solution_206.java
@@ -1,35 +1,19 @@
-package com.example.algorithm.leetcode.first;
-
-//Reverse a singly linked list.
-//
-// Example:
-//
-//
-//Input: 1->2->3->4->5->NULL
-//Output: 5->4->3->2->1->NULL
-//
-//
-// Follow up:
-//
-// A linked list can be reversed either iteratively or recursively. Could you implement both?
-// Related Topics Linked List
+package com.example.algorithm.leetcode.second;
 
 import com.example.algorithm.structure.ListNode;
 import com.example.algorithm.structure.SolutionUtil;
 
 public class Solution_206 {
     public static void main(String[] args) {
-        Solution_206 solution = new Solution_206();
-        ListNode list = SolutionUtil.createLink(new int[] {
+        Solution_206 solution_206 = new Solution_206();
+        ListNode result = solution_206.mySolution_1(SolutionUtil.createLink(new int[]{
                 1, 2, 3, 4, 5
-        });
-        solution.mySolution_1(list);
+        }));
     }
 
     public ListNode reverseList(ListNode head) {
         return null;
     }
-
 
     // 双指针反转法。
     public ListNode mySolution_1(ListNode head) {
