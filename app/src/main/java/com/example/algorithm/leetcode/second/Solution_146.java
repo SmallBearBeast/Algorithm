24b7eae735e474b25b278d6215b9c556950b4288
Hugo.Wu <smallbearbeast@gmail.com>
2023-08-18 13:36:14 +0800

    feat: Add second/Solution_146.java

--- app/src/main/java/com/example/algorithm/leetcode/second/Solution_146.java
+++ app/src/main/java/com/example/algorithm/leetcode/second/Solution_146.java
@@ -1,29 +1,4 @@
-package com.example.algorithm.leetcode.first;
-
-//运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
-//
-// 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
-//写入数据 put(key, value) - 如果密钥不存在，则写入其数据值。当缓存容量达到上限时，它应该在写入新数据之前删除最近最少使用的数据值，从而为新的数据值留出空间。
-//
-// 进阶:
-//
-// 你是否可以在 O(1) 时间复杂度内完成这两种操作？
-//
-// 示例:
-//
-// LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
-//
-//cache.put(1, 1);
-//cache.put(2, 2);
-//cache.get(1);       // 返回  1
-//cache.put(3, 3);    // 该操作会使得密钥 2 作废
-//cache.get(2);       // 返回 -1 (未找到)
-//cache.put(4, 4);    // 该操作会使得密钥 1 作废
-//cache.get(1);       // 返回 -1 (未找到)
-//cache.get(3);       // 返回  3
-//cache.get(4);       // 返回  4
-//
-// Related Topics 设计
+package com.example.algorithm.leetcode.second;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -31,29 +6,14 @@ import java.util.Map;
 
 public class Solution_146 {
     public static void main(String[] args) {
-        Solution_146 solution = new Solution_146();
-        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>(2, 0.75f, true);
-        map.put(1, 1);
-        map.put(2, 2);
-        map.put(3, 3);
-        map.get(2);
-
-        LRUCache_1 cache = new LRUCache_1(3 /* 缓存容量 */);
-        cache.put(1, 1);
-        cache.put(2, 2);
-        cache.put(3, 3);
-        cache.put(4, 4);
-        cache.get(4);
-        cache.get(3);
-        cache.get(2);
-        cache.get(1);
-        cache.put(5, 5);
-        cache.get(1);
-        cache.get(2);
-        cache.get(3);
-        cache.get(4);
-        cache.get(5);
+        Solution_146 solution_module = new Solution_146();
+        solution_module.mySolution_1();
     }
+
+    public void mySolution_1() {
+
+    }
+
 
     // 常规实现
     private static class LRUCache {
@@ -172,3 +132,4 @@ public class Solution_146 {
         }
     }
 }
+
