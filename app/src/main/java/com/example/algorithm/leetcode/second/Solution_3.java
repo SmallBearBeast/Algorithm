f2a1cfbb1a08e8eb2a51098a993038034188fb66
Hugo.Wu <smallbearbeast@gmail.com>
2023-08-18 14:54:31 +0800

    feat: Add second/Solution_3.java

--- app/src/main/java/com/example/algorithm/leetcode/second/Solution_3.java
+++ app/src/main/java/com/example/algorithm/leetcode/second/Solution_3.java
@@ -1,28 +1,6 @@
-package com.example.algorithm.leetcode.first;
-//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
-//
-// 示例 1:
-//
-// 输入: "abcabcbb"
-//输出: 3
-//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
-//
-//
-// 示例 2:
-//
-// 输入: "bbbbb"
-//输出: 1
-//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
-//
-//
-// 示例 3:
-//
-// 输入: "pwwkew"
-//输出: 3
-//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
-//
-// Related Topics 哈希表 双指针 字符串 Sliding Window
+package com.example.algorithm.leetcode.second;
+
+import com.example.algorithm.structure.SolutionUtil;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -30,10 +8,34 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+示例 1:
+
+输入: s = "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+提示：
+0 <= s.length <= 5 * 104
+s 由英文字母、数字、符号和空格组成
+ */
+
 public class Solution_3 {
     public static void main(String[] args) {
-        Solution_3 solution_3 = new Solution_3();
-        solution_3.mySolution_3("abcabcbb");
+        Solution_3 solution_module = new Solution_3();
+        SolutionUtil.print(solution_module.mySolution_1("pwwkew"));
     }
 
     // 滑动窗口法，本质就是判断每一个字符开头组成的最大无重复子串。
